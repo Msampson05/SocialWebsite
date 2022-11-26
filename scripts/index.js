064f5914410c14c11964b3d3b071866a2c7bbfd7
Megan Sampson <msampson@nyc.yearup.org>
2022-11-26 12:48:01 -0500

    completed javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,13 @@
 // SideBar 
 const menuItems = document.querySelectorAll('.menu-item');
 
+//messages
+
+const messagesNotification = document.querySelector('#message-notification');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
 // remove active class from all menu items 
 const changeActiveItem = () => {
     menuItems.forEach(item => {
@@ -18,8 +25,35 @@ menuItems.forEach(item => {
          } else{
                 document.querySelector('.notifications-popup').
                 style.display = 'block';
-                document.querySelector('#notifications . notification-count').style.display = 'none';
+                document.querySelector('#notifications .notification-count')
+                .style.display = 'none';
             }
         
     })
+})
+
+// =========================== Messages ======================= //
+//searches chat 
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            chat.style.display = 'flex';
+        } else{
+            chat.style.display = 'none;'
+        }
+
+    })
+}
+// search chat 
+messageSearch.addEventListener('keyup', searchMessage);
+
+// highlight messages card when messages menu item is clicked 
+messagesNotification.addEventListener('click' , () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
 })
